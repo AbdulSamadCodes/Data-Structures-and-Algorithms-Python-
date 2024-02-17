56fbe44ee5882d904eb4e806dc808f1b671c35ed
Samaadcodes <samadcom797@gmail.com>
2024-02-17 14:57:09 +0500

    2d array questions added

--- 2dimensional arrays.py
+++ 2dimensional arrays.py
@@ -84,6 +84,96 @@ import numpy as np
 # print(sineWaveprint(inputarray))
 
 
+
+#----------------------------Important Leetcode  problem-------------------------
+
+#:Write an efficient algoritm that seraches for a value in a n x m matrix.This matrix has following properties.
+#:Integers in each row are sorted from left to right
+#:The first integer of each row is greater than than last one of previous row.
+
+# def matSearch(matrix , target):
+#     row = len(matrix)
+#     col = len(matrix[0])
+
+#     start = 0
+#     end = row * col - 1
+
+#     mid = start + (end - start) // 2
+
+#     while start <= end:
+#         element = matrix[mid // col][mid % col]
+         
+#         if element == target:
+#             return 1
+#         elif element < target:
+#             start = mid + 1
+#         else:
+#             end = mid - 1
+
+#         mid = start + (end - start) // 2  
+
+#     return 0
+
+# matrix = [[1,4,5,6],
+#           [20,23,24,27],
+#           [29,31,34,36]]
+# target = 5
+# target2 = 30
+# print(matSearch(matrix , target))#:Output:1
+# print(matSearch(matrix,target2))#:Output:0
+
+
+
+#----------------------------Important Leetcode  problem-------------------------
+
+#:Given a matrix mat[][] of size N x M, where every row and column is sorted in increasing order, and a number X is given. The task is to find whether element X is present in the matrix or not.
+
+# def Search(matrix,M,N ,X):
+#     rowIndex = 0
+#     colIndex = M - 1
+
+#     while(rowIndex < N and colIndex >= 0):
+#         element = matrix[rowIndex][colIndex]
+        
+#         if element == X:
+#             return 1
+        
+#         elif element > X:
+#             colIndex-=1
+
+#         else:
+#             rowIndex+=1
+
+#     return 0
+
+
+
+# inputMatrix =  np.array([[1,4,7,11],
+#                          [2,5,8,12],
+#                          [3,6,9,21],
+#                          [8,9,678,689]])      
+
+# N = len(inputMatrix)
+# M = len(inputMatrix[0])
+# target = 9
+# print(Search(inputMatrix, M , N , target)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                
 
             
